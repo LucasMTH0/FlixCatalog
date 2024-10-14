7878117de82897104d6ab7ffde5ea6f56e03b068
unknown <lucas.matheus@ezoom.com.br>
2024-10-14 19:20:58 -0300

    removendo imports desnecessarios

--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
-import { Header } from './components/Header/index.tsx'
-import { Home } from './screens/Home/index.tsx'
-import { Detail } from './screens/Details/index.tsx'
-import { ThemeProvider } from 'styled-components'
-import theme from './theme/index.tsx'
-import { List } from './screens/List/index.tsx'
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { Header } from "./components/Header/index.tsx";
+import { Detail } from "./screens/Details/index.tsx";
+import { ThemeProvider } from "styled-components";
+import { Home } from "./screens/Home/index.tsx";
+import { List } from "./screens/List/index.tsx";
+import { createRoot } from "react-dom/client";
+import theme from "./theme/index.tsx";
+import { StrictMode } from "react";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     element: (
       <ThemeProvider theme={theme}>
-        <Header/>
-        <Outlet/>
+        <Header />
+        <Outlet />
       </ThemeProvider>
     ),
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/details/:id/:type',
-        element: <Detail />
+        path: "/details/:id/:type",
+        element: <Detail />,
       },
       {
-        path: '/list/:category',
-        element: <List/>
-      }
-    ]
-  }
+        path: "/list/:category",
+        element: <List />,
+      },
+    ],
+  },
+]);
 
-])
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
